5268614a62616a8ed8bb4a4d5f588cc8e19e33ab
Documentayshin <kyrmikee@gmail.com>
2024-12-15 12:01:06 -0500

    refactor: remove for clarity

--- src/main/java/dev/michaelodusami/organizationsystem/fields/ColumnType.java
+++
@@ -1,5 +0,0 @@
-package dev.michaelodusami.organizationsystem.fields;
-
-public enum ColumnType {
-    TEXT, NUMBER, PERCENTAGE, CURRENCY, DATE, UNCATEGORIZED
-}
--- src/organizationsystem-ui/src/routes/+page.svelte
+++ src/organizationsystem-ui/src/routes/+page.svelte
@@ -1,11 +1,3 @@
-<script>
-    import InstallationList from "../components/InstallationList.svelte";
-    import InstallationForm from "../components/InstallationForm.svelte";
-  </script>
-  
-  <main>
-    <h1>Installation Management System</h1>
-    <InstallationForm />
-    <InstallationList />
-  </main>
-  
+<main>
+  <h1>Hello World</h1>
+</main>
