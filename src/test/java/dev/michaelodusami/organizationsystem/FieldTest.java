5d660df6af9888d107dada01427779cf62122c3a
Documentayshin <kyrmikee@gmail.com>
2024-12-15 01:20:20 -0500

    test: Add tests for the Field Column

--- src/test/java/dev/michaelodusami/organizationsystem/FieldTest.java
+++ src/test/java/dev/michaelodusami/organizationsystem/FieldTest.java
@@ -2,6 +2,7 @@ package dev.michaelodusami.organizationsystem;
 
 import org.junit.jupiter.api.Test;
 
+import dev.michaelodusami.organizationsystem.fields.ColumnType;
 import dev.michaelodusami.organizationsystem.fields.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,8 +13,8 @@ public class FieldTest {
 
     @Test
     public void testTextField() {
-        Field textField = new Field(Field.Type.TEXT, "Hello, World!");
-        assertEquals(Field.Type.TEXT, textField.getType());
+        Field textField = new Field(ColumnType.TEXT, "Hello, World!");
+        assertEquals(ColumnType.TEXT, textField.getType());
         assertEquals("Hello, World!", textField.getValue());
 
         textField.setValue("New Value");
@@ -28,8 +29,8 @@ public class FieldTest {
 
     @Test
     public void testNumberField() {
-        Field numberField = new Field(Field.Type.NUMBER, 42);
-        assertEquals(Field.Type.NUMBER, numberField.getType());
+        Field numberField = new Field(ColumnType.NUMBER, 42);
+        assertEquals(ColumnType.NUMBER, numberField.getType());
         assertEquals(42, numberField.getValue());
 
         numberField.setValue(99.99);
@@ -38,22 +39,22 @@ public class FieldTest {
 
     @Test
     public void testTypeConversion() {
-        Field field = new Field(Field.Type.TEXT, "123");
-        field.setType(Field.Type.NUMBER); // Should convert the value to a number
+        Field field = new Field(ColumnType.TEXT, "123");
+        field.setType(ColumnType.NUMBER); // Should convert the value to a number
         assertEquals(123.0, field.getValue()); // Automatically converted
 
         // Test invalid conversion
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            Field invalidField = new Field(Field.Type.TEXT, "Invalid");
-            invalidField.setType(Field.Type.NUMBER); // Should fail
+            Field invalidField = new Field(ColumnType.TEXT, "Invalid");
+            invalidField.setType(ColumnType.NUMBER); // Should fail
         });
         assertTrue(exception.getMessage().contains("Unable to convert value to the new type"));
     }
 
     @Test
     public void testPercentageField() {
-        Field percentageField = new Field(Field.Type.PERCENTAGE, 85.5);
-        assertEquals(Field.Type.PERCENTAGE, percentageField.getType());
+        Field percentageField = new Field(ColumnType.PERCENTAGE, 85.5);
+        assertEquals(ColumnType.PERCENTAGE, percentageField.getType());
         assertEquals(85.5, percentageField.getValue());
 
         // Test out-of-range percentage
@@ -66,8 +67,8 @@ public class FieldTest {
     @Test
     public void testDateField() {
         Date currentDate = new Date();
-        Field dateField = new Field(Field.Type.DATE, currentDate);
-        assertEquals(Field.Type.DATE, dateField.getType());
+        Field dateField = new Field(ColumnType.DATE, currentDate);
+        assertEquals(ColumnType.DATE, dateField.getType());
         assertEquals(currentDate, dateField.getValue());
 
         // Test invalid date
